a49a321068374b51693dd57b240151c28b36231e
andrew-youngjae <andrewlyj1@gmail.com>
2024-11-19 13:59:10 +0900

    Initial Commit

---
+++ ARRulerTests/ARRulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARRulerTests.swift
+//  ARRulerTests
+//
+//  Created by YoungJae Lee on 11/19/24.
+//
+
+import Testing
+@testable import ARRuler
+
+struct ARRulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
